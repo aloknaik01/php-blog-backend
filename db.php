babc71094f84597c5b66216005308be928a8528e
aloknaik <aloknaik016261@gmail.com>
2025-08-03 00:08:12 +0530

    Add DB connection and test file

--- db.php
+++ db.php
@@ -2,10 +2,12 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "blog_db";
+$database = "blog_db";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
