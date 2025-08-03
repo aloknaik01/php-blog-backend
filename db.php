b8091cf8428e95ba040b46913e2921442daf2f6f
aloknaik <aloknaik016261@gmail.com>
2025-08-04 00:15:14 +0530

    Secure DB credentials using .env and dotenv

--- db.php
+++ db.php
@@ -1,15 +1,23 @@
+
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "blog_db";
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$database = $_ENV['DB_NAME'];
+
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($host, $user, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
 
